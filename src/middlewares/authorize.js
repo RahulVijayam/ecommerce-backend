2d53ea775d39f4f74492cc7f3e50716fc6b2434a
Rahul Vijayanagaram <rahulvijayanagarm@gmail.com>
2026-08-19 08:45:08 +0530

    Implemented Authorization Middleware

---
+++ src/middlewares/authorize.js
@@ -0,0 +1,23 @@
+const User = require('../models/User')
+
+
+const verifyRole = async(req,res,next) => {
+    userId = req.user.userId
+
+    try {
+    const user = await User.findOne({userId})
+
+    if(user.role=='admin'){
+        next()
+    }
+}
+catch(error){
+    return res.status(401).json[{message:"Unauthorized Access"}]
+
+}
+    
+
+}
+
+
+module.exports=verifyRole
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,13 +2,14 @@ const express = require('express');
 const userController = require('../controllers/userController');
 const {signupRules,loginRules}=require('../middlewares/userValidator')
 const verifyToken = require('../middlewares/authMiddleware')
+const verifyUser = require('../middlewares/authorize')
 
 const router=express.Router();
 
 
 router.post('/register',signupRules,userController.register);
 router.post('/login',loginRules,userController.login);
-router.get('/profile', verifyToken, userController.profile);
+router.get('/profile', verifyToken,verifyUser, userController.profile);
 
 
 module.exports =router; 
